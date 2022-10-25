a8c4b625c91eaef8108bf3b948b6801257c25b87
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2022-10-25 17:22:38 -0500

    add variables to javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const wrapper = document.querySelector(".wrapper"),
+inputPart = document.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+weatherPart = wrapper.querySelector(".weather-part"),
+wIcon = weatherPart.querySelector("img"),
+arrowBack = wrapper.querySelector("header i");
+
+
+
+let api;
